6263b659248d30dc0d5c4bede1f774f9ada8ff6f
Luis Lopez <llopez1518@gmail.com>
2022-01-06 19:42:36 -0700

    revise back button functionality and next button change

--- quiz-app-project/main.js
+++ quiz-app-project/main.js
@@ -1,7 +1,6 @@
 const quizDB = [
   {
-    question:
-      "Question 1: Which of the following is not a real eCommerce platform?",
+    question: "Which of the following is not a real eCommerce platform?",
     a: "Shopify",
     b: "WooCommerce",
     c: "ShopCommerce",
@@ -9,8 +8,7 @@ const quizDB = [
     ans: "ans3",
   },
   {
-    question:
-      "Question 2: If Shopify is so good, why are Shopify developers necessary?",
+    question: "If Shopify is so good, why are Shopify developers necessary?",
     a: "To save time on things like store setups and migrations",
     b: "To extend the limited design options and functionalities of themes with custom code",
     c: "To provide support with a deep understanding of how the platform works and what its limitations are",
@@ -18,8 +16,7 @@ const quizDB = [
     ans: "ans4",
   },
   {
-    question:
-      "Question 3: Which of the following is true about Shopify developers?",
+    question: "Which of the following is true about Shopify developers?",
     a: "They are paid extremely well",
     b: "There is a high demand for them",
     c: "They need to know web development, the platform itself, and the liquid template language",
@@ -28,29 +25,49 @@ const quizDB = [
   },
 ];
 
+const questionCounter = document.querySelector(".question-count");
 const question = document.querySelector(".question");
-const Option1 = document.querySelector("#option1");
-const Option2 = document.querySelector("#option2");
-const Option3 = document.querySelector("#option3");
-const Option4 = document.querySelector("#option4");
-const Submit = document.querySelector("#submit");
+const option1 = document.querySelector("#option1");
+const option2 = document.querySelector("#option2");
+const option3 = document.querySelector("#option3");
+const option4 = document.querySelector("#option4");
+const next = document.querySelector("#next");
+const error = document.querySelector(".error");
+const back = document.querySelector("#back");
+const submit = document.querySelector("submit");
 const answers = document.querySelectorAll(".answer");
 const totalScore = document.querySelector("#showScore");
 
 let score = 0;
 let questionCount = 0;
 
+const showBackBtn = () => {
+  if (questionCount >= 1) {
+    back.style.display = "inline";
+  } else {
+    back.style.display = "none";
+  }
+};
+
 const loadQuestions = () => {
+  showBackBtn();
+  
+  questionCounter.innerText = `Question ${questionCount + 1} of ${
+    quizDB.length
+  }:`;
   const questionList = quizDB[questionCount];
   question.innerText = questionList.question;
-  Option1.innerText = questionList.a;
-  Option2.innerText = questionList.b;
-  Option3.innerText = questionList.c;
-  Option4.innerText = questionList.d;
+  option1.innerText = questionList.a;
+  option2.innerText = questionList.b;
+  option3.innerText = questionList.c;
+  option4.innerText = questionList.d;
+  error.style.display = "none";
 };
 loadQuestions();
 
-const getCheckAnswer = () => {
+
+
+const getCheckedAnswer = () => {
   let answer;
   answers.forEach((currentAns) => {
     if (currentAns.checked) {
@@ -60,27 +77,40 @@ const getCheckAnswer = () => {
   return answer;
 };
 
-const deSelectAll = () => {
+const noCheckNoAdvance = () => {
+  answers.forEach((currentAns) => {
+    if (currentAns.value === "on") {
+      error.style.display = "block";
+    }
+  });
+};
+
+const removeCheck = () => {
   answers.forEach((currentAns) => {
     currentAns.checked = false;
   });
 };
 
-Submit.addEventListener("click", () => {
-  const checkAnswer = getCheckAnswer();
+next.addEventListener("click", () => {
+  const checkAnswer = getCheckedAnswer();
 
   if (checkAnswer === quizDB[questionCount].ans) {
     score++;
   }
-
   questionCount++;
 
-  deSelectAll();
+  if (questionCount === quizDB.length - 1) {
+    next.innerText = "Submit";
+  }
+  //noCheckNoAdvance();
+  removeCheck();
   if (questionCount < quizDB.length) {
     loadQuestions();
   } else {
     totalScore.classList.remove("scoreArea");
     totalScore.innerHTML = `<h3>Your total score is:<span id='score'>${score}</span></h3>
         <button onclick='location.reload()' >Try again?</button>`;
+    next.style.display = "none";
+    back.style.display = "none";
   }
 });
